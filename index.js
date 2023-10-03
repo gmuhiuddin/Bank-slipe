eeeb8a584cb0283ec7aa780ed6f318530f10e6a0
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-10-03 18:21:48 +0500

    added changes

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ button.style.borderColor = 'white'
 button.style.boxShadow = '1px 1px 3px white'
 
 
-}else if(body[0].style.backgroundColor == 'black'){
+}else{
     body[0].style.backgroundColor = 'white';
 
     let slipeDiv = document.getElementsByClassName('slipe-div'); 
