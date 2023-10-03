d1b2966ed05855205a35689c9408588019175b60
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-10-03 17:42:23 +0500

    added theme change buttom

--- index.js
+++ index.js
@@ -22,7 +22,52 @@ minuts.innerText = NowDate.getMinutes()
 let counting = Math.random()*99
 let minus = Math.random()*6
 let yourNumberZero = document.getElementById('yourNumberZero')
-yourNumberZero.innerText = counting.toFixed(0)
+yourNumberZero.innerText = Math.round(counting)
 
 let nowServingZero = document.getElementById('nowServingZero');
-nowServingZero.innerText = counting.toFixed(0) - minus.toFixed(0) 
+nowServingZero.innerText = Math.round(counting) - Math.round(minus++)
+
+let button = document.getElementById('theme-changer-btn');
+button.addEventListener('click',themeChanger)
+
+function themeChanger (){
+    let body = document.getElementsByTagName('body')
+
+    if(body[0].style.backgroundColor !== 'black'){
+
+body[0].style.backgroundColor = 'black';
+
+let slipeDiv = document.getElementsByClassName('slipe-div'); 
+slipeDiv[0].style.borderColor = 'white'
+slipeDiv[0].style.boxShadow = '1px 1px 13px white'
+slipeDiv[0].style.color = 'white'
+slipeDiv[0].style.backgroundColor = 'black'
+
+let headings = document.getElementsByClassName('headings')
+headings[0].style.borderColor = 'white'
+
+button.style.backgroundColor = 'black'
+button.style.color = 'white'
+button.style.borderColor = 'white'
+button.style.boxShadow = '1px 1px 3px white'
+
+
+}
+
+else if (body[0].style.backgroundColor == 'black'){
+    body[0].style.backgroundColor = 'white';
+
+    let slipeDiv = document.getElementsByClassName('slipe-div'); 
+    slipeDiv[0].style.borderColor = 'black'
+    slipeDiv[0].style.color = 'black'
+    slipeDiv[0].style.backgroundColor = 'white'
+    
+    let headings = document.getElementsByClassName('headings')
+    headings[0].style.borderColor = 'black'
+    
+    button.style.backgroundColor = 'white'
+    button.style.color = 'black'
+    button.style.borderColor = 'black'
+      
+}
+}
