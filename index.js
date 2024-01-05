3e21f089244a91dd7aeeb58a846c58029c762c7e
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-01-05 20:33:00 +0500

    add some change

--- index.js
+++ index.js
@@ -1,71 +1,62 @@
 let year = document.getElementById('year');
 let month = document.getElementById('month');
 let day = document.getElementById('day');
-
-
+let button = document.getElementById('theme-changer-btn');
 let hour = document.getElementById('hour');
 let minuts = document.getElementById('minuts');
-
-let NowDate = new Date()
-
-year.innerText  = NowDate.getFullYear()
-
-month.innerText = NowDate.getMonth()+1
-
-day.innerText = NowDate.getDate()
-
-hour.innerText = NowDate.getHours()
-
-minuts.innerText = NowDate.getMinutes()
-
-
-let counting = Math.random()*99
-let minus = Math.random()*6
-let yourNumberZero = document.getElementById('yourNumberZero')
-yourNumberZero.innerText = Math.round(counting)
-
+let yourNumberZero = document.getElementById('yourNumberZero');
 let nowServingZero = document.getElementById('nowServingZero');
-nowServingZero.innerText = Math.round(counting) - Math.round(minus++)
+let counting = Math.random() * 99;
+let minus = Math.random() * 6;
+let NowDate = new Date();
 
-let button = document.getElementById('theme-changer-btn');
-button.addEventListener('click',themeChanger)
+year.innerText = NowDate.getFullYear();
+month.innerText = NowDate.getMonth() + 1;
+day.innerText = NowDate.getDate();
+hour.innerText = NowDate.getHours();
+minuts.innerText = NowDate.getMinutes();
 
-function themeChanger (){
+yourNumberZero.innerText = Math.round(counting);
+nowServingZero.innerText = Math.round(counting) - Math.round(minus++);
+
+button.addEventListener('click', themeChanger);
+
+function themeChanger() {
     let body = document.getElementsByTagName('body')
 
-    if(body[0].style.backgroundColor !== 'black'){
+    if (body[0].style.backgroundColor !== 'black') {
 
-body[0].style.backgroundColor = 'black';
+        body[0].style.backgroundColor = 'black';
 
-let slipeDiv = document.getElementsByClassName('slipe-div'); 
-slipeDiv[0].style.borderColor = 'white'
-slipeDiv[0].style.boxShadow = '1px 1px 13px white'
-slipeDiv[0].style.color = 'white'
-slipeDiv[0].style.backgroundColor = 'black'
+        let slipeDiv = document.getElementsByClassName('slipe-div');
+        slipeDiv[0].style.borderColor = 'white'
+        slipeDiv[0].style.boxShadow = '1px 1px 13px white'
+        slipeDiv[0].style.color = 'white'
+        slipeDiv[0].style.backgroundColor = 'black'
 
-let headings = document.getElementsByClassName('headings')
-headings[0].style.borderColor = 'white'
+        let headings = document.getElementsByClassName('headings')
+        headings[0].style.borderColor = 'white'
 
-button.style.backgroundColor = 'black'
-button.style.color = 'white'
-button.style.borderColor = 'white'
-button.style.boxShadow = '1px 1px 3px white'
+        button.style.backgroundColor = 'black'
+        button.style.color = 'white'
+        button.style.borderColor = 'white'
+        button.style.boxShadow = '1px 1px 3px white'
 
 
-}else{
-    body[0].style.backgroundColor = 'white';
+    } else {
+        body[0].style.backgroundColor = 'white';
 
-    let slipeDiv = document.getElementsByClassName('slipe-div'); 
-    slipeDiv[0].style.borderColor = 'black'
-    slipeDiv[0].style.color = 'black'
-    slipeDiv[0].style.backgroundColor = 'white'
-    
-    let headings = document.getElementsByClassName('headings')
-    headings[0].style.borderColor = 'black'
-    
-    button.style.backgroundColor = 'white'
-    button.style.color = 'black'
-    button.style.borderColor = 'black'
-      
-}
+        let slipeDiv = document.getElementsByClassName('slipe-div');
+        slipeDiv[0].style.borderColor = 'black'
+        slipeDiv[0].style.color = 'black'
+        slipeDiv[0].style.backgroundColor = 'white'
+
+        let headings = document.getElementsByClassName('headings')
+        headings[0].style.borderColor = 'black'
+
+        button.style.backgroundColor = 'white'
+        button.style.color = 'black'
+        button.style.borderColor = 'black'
+
+    }
 }
